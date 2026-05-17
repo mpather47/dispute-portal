9595bfaf840292d235a33a1109bddb822334871f
mpather47 <mpather47@gmail.com>
2026-05-17 21:19:59 +0200

    notification toasts and badge

--- dispute-portal-client/src/App.tsx
+++ dispute-portal-client/src/App.tsx
@@ -2,6 +2,9 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import ErrorBoundary from "./components/ErrorBoundary";
+import NotificationToaster from "./components/NotificationToaster";
+import { NotificationContext } from "./context/NotificationContext";
+import { useNotificationPolling } from "./hooks/useNotificationPolling";
 import LoginPage from "./pages/LoginPage";
 import TransactionsPage from "./pages/TransactionsPage";
 import CreateDisputePage from "./pages/CreateDisputePage";
@@ -11,68 +14,73 @@ import AdminDisputesPage from "./pages/AdminDisputesPage";
 import NotificationsPage from "./pages/NotificationsPage";
 
 export default function App() {
+  const { toasts, unreadCount, dismiss, markAllRead } = useNotificationPolling();
+
   return (
-    <ErrorBoundary>
-      <Navbar />
+    <NotificationContext.Provider value={{ unreadCount, markAllRead }}>
+      <ErrorBoundary>
+        <Navbar />
+        <NotificationToaster toasts={toasts} onDismiss={dismiss} />
 
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<LoginPage />} />
 
-        <Route
-          path="/transactions"
-          element={
-            <ProtectedRoute allowedRoles={["Customer"]}>
-              <TransactionsPage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/transactions"
+            element={
+              <ProtectedRoute allowedRoles={["Customer"]}>
+                <TransactionsPage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/transactions/:id/dispute"
-          element={
-            <ProtectedRoute allowedRoles={["Customer"]}>
-              <CreateDisputePage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/transactions/:id/dispute"
+            element={
+              <ProtectedRoute allowedRoles={["Customer"]}>
+                <CreateDisputePage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/my-disputes"
-          element={
-            <ProtectedRoute allowedRoles={["Customer"]}>
-              <MyDisputesPage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/my-disputes"
+            element={
+              <ProtectedRoute allowedRoles={["Customer"]}>
+                <MyDisputesPage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/disputes/:id"
-          element={
-            <ProtectedRoute allowedRoles={["Customer", "Admin"]}>
-              <DisputeDetailsPage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/disputes/:id"
+            element={
+              <ProtectedRoute allowedRoles={["Customer", "Admin"]}>
+                <DisputeDetailsPage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/admin/disputes"
-          element={
-            <ProtectedRoute allowedRoles={["Admin"]}>
-              <AdminDisputesPage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/admin/disputes"
+            element={
+              <ProtectedRoute allowedRoles={["Admin"]}>
+                <AdminDisputesPage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/notifications"
-          element={
-            <ProtectedRoute allowedRoles={["Customer", "Admin"]}>
-              <NotificationsPage />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </ErrorBoundary>
+          <Route
+            path="/notifications"
+            element={
+              <ProtectedRoute allowedRoles={["Customer", "Admin"]}>
+                <NotificationsPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </ErrorBoundary>
+    </NotificationContext.Provider>
   );
 }
